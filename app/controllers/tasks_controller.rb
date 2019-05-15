89caec9885059264bface61ff8304fe320e29d97
moral nicolas <mrwallp@gmail.com>
2019-05-15 11:07:05 +0200

    rtm

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,39 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(set_task_params)
+    @task.save
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(set_task_params)
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+
+  def set_task_params
+    params.require(:task).permit(:title, :details, :checkbox_value)
+  end
+
+  def set_task
     @task = Task.find(params[:id])
   end
 end
